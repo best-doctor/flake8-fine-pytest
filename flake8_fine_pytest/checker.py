0f9c1bcc73835785c0a0ba402c19ddd20ddfa6d5
katerina <you@example.com>
2020-05-12 21:07:01 +0300

    added error format and run yield type

--- flake8_fine_pytest/checker.py
+++ flake8_fine_pytest/checker.py
@@ -4,6 +4,8 @@ from flake8_fine_pytest import __version__ as version
 
 from flake8_fine_pytest.ast_helpers import get_wrong_xfail_decorator_lines
 
+ErrorType = typing.Tuple[int, int, str, type]
+
 
 class FinePytestChecker:
     name = 'flake8-fine-pytest'
@@ -13,25 +15,33 @@ class FinePytestChecker:
         self.filename = filename
         self.tree = tree
 
-    def run(self) -> typing.Generator[str, None, None]:
+    def run(self) -> typing.Generator[ErrorType, None, None]:
         xfail_with_empty_reason, xfail_without_reason = get_wrong_xfail_decorator_lines(self.tree)
         errors = self.get_xfail_reason_errors(
             xfail_with_empty_reason,
             xfail_without_reason,
         )
         for error in errors:
-            yield error
+            yield (*error, type(self))
 
     def get_xfail_reason_errors(
         self,
         xfail_with_empty_reason: typing.Set[int],
         xfail_without_reason: typing.Set[int],
-    ) -> typing.List[str]:
+    ) -> typing.List[typing.Tuple[int, int, str]]:
         errors = []
         if xfail_with_empty_reason or xfail_without_reason:
 
             for line in xfail_with_empty_reason:
-                errors.append(f'{self.filename}:{line} FP001 xfailed test with empty reason')
+                errors.append((
+                    line,
+                    1,
+                    'FP001 xfailed test with empty reason'
+                ))
             for line in xfail_without_reason:
-                errors.append(f'{self.filename}:{line} FP002 xfailed test without reason')
+                errors.append((
+                    line,
+                    1,
+                    'FP001 xfailed test with empty reason'
+                ))
         return errors
