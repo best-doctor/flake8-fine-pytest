76295e638cf17cd6282d7882ae19c2277872ef7e
lowit <ea@lowit.ru>
2020-11-29 01:33:59 +0300

    unique_tests: deletes typing in the testfile

--- tests/test_files/test_not_unique.py
+++ tests/test_files/test_not_unique.py
@@ -1,19 +1,19 @@
 import pytest
 
 
-def test_not_uniq() -> None:
+def test_not_uniq():
     pass
 
 
-def test_not_uniq() -> None:
+def test_not_uniq():
     pass
 
 
 @pytest.mark.parametrize('test', ('test',))
-def test_not_uniq_with_decorator(test) -> None:
+def test_not_uniq_with_decorator(test):
     pass
 
 
 @pytest.mark.parametrize('test', ('test2',))
-def test_not_uniq_with_decorator(test) -> None:
+def test_not_uniq_with_decorator(test):
     pass
