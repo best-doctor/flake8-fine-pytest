c3b665e79f860f960aa5ee80f6b28eed4542228b
Mike Koltsov <mikekoltsov@gmail.com>
2020-11-30 14:26:21 +0300

    add test class test for usefixtures

--- tests/test_integration/test_usefixtures.py
+++ tests/test_integration/test_usefixtures.py
@@ -10,6 +10,20 @@ def test_fixtures_should_be_in_usefixtures(run_validator_for_test_files):
     assert errors[0][2] == expected_error_message
 
 
+def test_testclass_self_or_cls_not_included_into_fixtures_list(
+    run_validator_for_test_files,
+):
+    expected_error_message = (
+        'FP009 test_one should use '
+        "fixtures as follows: @pytest.mark.usefixtures('capsys')"
+    )
+
+    errors = run_validator_for_test_files('test_class_with_fixtures.py')
+
+    assert len(errors) == 1
+    assert errors[0][2] == expected_error_message
+
+
 def test_fixtures_should_be_in_usefixtures_but_validator_is_disabled(
     run_validator_for_test_files,
 ):
